53c2d6ff924c5174d3d81e4482413beb73c7812a
Aaron Randall <aaronjrandall@gmail.com>
2013-01-06 01:07:14 +0000

    Test app

---
+++ pledgebench/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pledgebench::Application.config.secret_token = '29d0c815fc1618afb79cad5a6915116c95899b841307e9816707ad5e086c4e0b6770296068dc7dab78e39392b249566b527cd1dc5c41f525aad2a96d3ef2ab1f'
