dcff3887648906245a0f825493ba70c9c12d78c0
Aaron Randall <aaronjrandall@gmail.com>
2013-09-10 18:01:55 -0300

    Added empty rails 4 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pledgebench::Application.config.secret_key_base = 'a915af54ac035190631cdd39b9c7533ca5e388e5d476a8bf475bf98858d0bad697286bbc23cca9b3a52f934ed8a69e4cfa16bcb435d703e8f8f19fa924804e7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pledgebench::Application.config.session_store :cookie_store, key: '_pledgebench_session'
